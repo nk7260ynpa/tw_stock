f03f6371783d40f779e9de46b9ded0aa6253f7f1
nk7260ynpa <nk7260ynpa@gmail.com>
2024-02-25 00:59:23 +0800

    Update pytest

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,23 +5,3 @@ import twse_crawler
 def test_gen_date_list():
     assert utils.gen_date_list("2021-01-01", "2021-01-05") == ['2021-01-01', '2021-01-02', '2021-01-03', '2021-01-04']
 
-def test_holiday_crawler():
-    date = "2024-01-01"
-    df = twse_crawler.crawler_twse(date)
-    assert isinstance(df, pd.DataFrame)
-
-def test_weekend_crawler():
-    date = "2024-01-13"
-    df = twse_crawler.crawler_twse(date)
-    assert isinstance(df, pd.DataFrame)
-
-def test_week_crawler():
-    date = "2024-01-12"
-    df = twse_crawler.crawler_twse(date)
-    assert isinstance(df, pd.DataFrame)
-
-def test_en_date_list():
-    start_date = "2024-01-30"
-    end_date = "2024-02-02"
-    date_list = utils.gen_date_list(start_date, end_date)
-    assert date_list == ["2024-01-30", "2024-01-31", "2024-02-01"]
