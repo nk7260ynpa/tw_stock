46e30ba3e492061f548112cd6feb2ac1bf56a17a
nk7260ynpa <nk7260ynpa@gmail.com>
2024-02-25 00:32:56 +0800

    Add pytest function

--- utils.py
+++ utils.py
@@ -5,6 +5,3 @@ def gen_date_list(start_date, end_date):
     end_date = datetime.datetime.strptime(end_date, "%Y-%m-%d")
     date_list = [(start_date + datetime.timedelta(days=x)).strftime("%Y-%m-%d") for x in range(0, (end_date-start_date).days)]
     return date_list
-
-if __name__ == "__main__":
-    print(date_list("2021-01-01", "2021-01-05"))
